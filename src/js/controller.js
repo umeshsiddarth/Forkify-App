2b28ae36f2f3db120e8a2abfaf231b02f03fbfad
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-02-05 20:10:47 +0530

    Initialized with parcel and loaded a recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +11,33 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+    );
+
+    const { data, message } = await res.json();
+    if (!res.ok) throw new Error(`${message} ${res.status}`);
+    // console.log(data.recipe);
+    // console.log(res);
+
+    let { recipe } = data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+showRecipe();
