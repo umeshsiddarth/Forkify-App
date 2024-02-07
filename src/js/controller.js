cf7cea8b25f4f439a364b4e63f48cc391fcc908b
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-02-07 12:27:28 +0530

    Added Event Listener to show recipes based on load and hashchange events

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,9 +30,13 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return renderSpinner(recipeContainer);
+
     renderSpinner(recipeContainer);
+
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     const { data, message } = await res.json();
@@ -52,78 +56,81 @@ const showRecipe = async function () {
       ingredients: recipe.ingredients,
     };
 
-    const html = `<figure class="recipe__fig">
-    <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
-    <h1 class="recipe__title">
-      <span>${recipe.title}</span>
-    </h1>
-  </figure>
+    const html = `
+        <figure class="recipe__fig">
+          <img src="${recipe.image}" alt="${
+      recipe.title
+    }" class="recipe__img" />
+          <h1 class="recipe__title">
+            <span>${recipe.title}</span>
+          </h1>
+        </figure>
 
-  <div class="recipe__details">
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="${icons}#icon-clock"></use>
-      </svg>
-      <span class="recipe__info-data recipe__info-data--minutes">${
-        recipe.cookingTime
-      }</span>
-      <span class="recipe__info-text">minutes</span>
-    </div>
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="${icons}#icon-users"></use>
-      </svg>
-      <span class="recipe__info-data recipe__info-data--people">${
-        recipe.servings
-      }</span>
-      <span class="recipe__info-text">servings</span>
-
-      <div class="recipe__info-buttons">
-        <button class="btn--tiny btn--increase-servings">
-          <svg>
-            <use href="${icons}#icon-minus-circle"></use>
+        <div class="recipe__details">
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="${icons}#icon-clock"></use>
+            </svg>
+          <span class="recipe__info-data recipe__info-data--minutes">${
+            recipe.cookingTime
+          }</span>
+          <span class="recipe__info-text">minutes</span>
+        </div>
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${icons}#icon-users"></use>
           </svg>
-        </button>
-        <button class="btn--tiny btn--increase-servings">
+          <span class="recipe__info-data recipe__info-data--people">${
+            recipe.servings
+          }</span>
+          <span class="recipe__info-text">servings</span>
+
+          <div class="recipe__info-buttons">
+            <button class="btn--tiny btn--increase-servings">
+              <svg>
+                <use href="${icons}#icon-minus-circle"></use>
+              </svg>
+            </button>
+            <button class="btn--tiny btn--increase-servings">
+              <svg>
+                <use href="${icons}#icon-plus-circle"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div class="recipe__user-generated">
           <svg>
-            <use href="${icons}#icon-plus-circle"></use>
+            <use href="${icons}#icon-user"></use>
+          </svg>
+        </div>
+        <button class="btn--round">
+          <svg class="">
+            <use href="${icons}#icon-bookmark-fill"></use>
           </svg>
         </button>
       </div>
-    </div>
 
-    <div class="recipe__user-generated">
-      <svg>
-        <use href="${icons}#icon-user"></use>
-      </svg>
-    </div>
-    <button class="btn--round">
-      <svg class="">
-        <use href="${icons}#icon-bookmark-fill"></use>
-      </svg>
-    </button>
-  </div>
-
-  <div class="recipe__ingredients">
-    <h2 class="heading--2">Recipe ingredients</h2>
-    <ul class="recipe__ingredient-list">
-    ${recipe.ingredients
-      .map((ing) => {
-        // As we need to return a string we use map instead of forEach
-        return `
-      <li class="recipe__ingredient">
-        <svg class="recipe__icon">
-          <use href="src/img/icons.svg#icon-check"></use>
-        </svg>
-        <div class="recipe__quantity">${ing.quantity}</div>
-        <div class="recipe__description">
-          <span class="recipe__unit">${ing.unit}</span>
-          ${ing.description}
-        </div>
-      </li>
+      <div class="recipe__ingredients">
+        <h2 class="heading--2">Recipe ingredients</h2>
+        <ul class="recipe__ingredient-list">
+        ${recipe.ingredients
+          .map((ing) => {
+            // As we need to return a string we use map instead of forEach
+            return `
+          <li class="recipe__ingredient">
+            <svg class="recipe__icon">
+              <use href="src/img/icons.svg#icon-check"></use>
+            </svg>
+            <div class="recipe__quantity">${ing.quantity}</div>
+            <div class="recipe__description">
+              <span class="recipe__unit">${ing.unit}</span>
+              ${ing.description}
+            </div>
+          </li>
       `;
-      })
-      .join("")}
+          })
+          .join("")}
       
 
       
@@ -159,4 +166,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+["hashchange", "load"].forEach((event) =>
+  window.addEventListener(event, showRecipe)
+);
